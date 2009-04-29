e78329d0542ff21d6e4411430f4bbf2a2bd9670b
shemnon <danno.ferrin@shemnon.com>
2009-04-28 22:40:33 -0600

    step two: put tweets on the map

--- griffon-app/controllers/TwittersphereController.groovy
+++ griffon-app/controllers/TwittersphereController.groovy
@@ -1,14 +1,37 @@
+import gov.nasa.worldwind.event.SelectEvent
+import gov.nasa.worldwind.geom.Position
+import gov.nasa.worldwind.geom.Angle
+import gov.nasa.worldwind.globes.Globe
+import gov.nasa.worldwind.view.OrbitView
+import gov.nasa.worldwind.view.FlyToOrbitViewStateIterator
+
 class TwittersphereController {
-    // these will be injected by Griffon
-    def model
-    def view
+
+    TwittersphereModel model
+    TwittersphereView view
 
     void mvcGroupInit(Map args) {
         // this method is called after model and view are injected
     }
 
-    /*
-    def action = { evt = null ->
+    def mapSelection = { SelectEvent event ->
+        println "Try!"
+        if ((event.eventAction == SelectEvent.LEFT_CLICK)
+            && (event.topPickedObject?.hasPosition)
+            && (model.wwd.view instanceof OrbitView))
+        {
+            OrbitView orbitView = model.wwd.view
+            Globe globe = model.wwd.model.globe;
+            if (globe != null && orbitView != null) {
+                println "Success!"
+                Position targetPos = event.topPickedObject.position
+                // Use a PanToIterator to iterate view to target position
+                orbitView.applyStateIterator(FlyToOrbitViewStateIterator.createPanToIterator(
+                    // The elevation component of 'targetPos' here is not the surface elevation,
+                    // so we ignore it when specifying the view center position.
+                    view, globe, new Position(targetPos, 0),
+                    Angle.ZERO, Angle.ZERO, targetPos.getElevation() + this.elevationOffset));
+            }
+        }
     }
-    */
 }
--- griffon-app/views/TwittersphereView.groovy
+++ griffon-app/views/TwittersphereView.groovy
@@ -1,15 +1,63 @@
+import java.awt.Color
 import gov.nasa.worldwind.awt.WorldWindowGLCanvas
 import gov.nasa.worldwind.layers.WorldMapLayer
 import gov.nasa.worldwind.examples.ClickAndGoSelectListener
 import gov.nasa.worldwind.examples.LayerPanel
 import java.awt.BorderLayout
+import gov.nasa.worldwind.event.SelectListener
+import java.awt.image.BufferedImage
+import gov.nasa.worldwind.geom.Position
+import java.awt.Image
+import gov.nasa.worldwind.render.GlobeAnnotation
+import gov.nasa.worldwind.render.Annotation
+import gov.nasa.worldwind.layers.RenderableLayer
+import gov.nasa.worldwind.layers.AnnotationLayer
 
 application(title:'twittersphere',  pack:true, locationByPlatform:true) {
 
     wwd = widget(new WorldWindowGLCanvas(), preferredSize: [500, 500],
-        model: model.worldWindModel, constraints: BorderLayout.CENTER)
-    wwd.addSelectListener(new ClickAndGoSelectListener(wwd, WorldMapLayer.class));
+        model: model.worldWindModel, constraints: BorderLayout.CENTER,
+        selected: controller.mapSelection 
+    )
+    println 'hi'
+    wwd.addSelectListener (controller.mapSelection as SelectListener)
+    wwd.inputHandler.mouseClicked = { evt ->
+        println evt
+        icon = imageIcon('/griffon-icon-48x48.png')
+        addTweet(Position.fromDegrees(38.9666, -104.7227, 0), "Tweet!", icon.image);
+    }
+    //wwd.addSelectListener(new ClickAndGoSelectListener(wwd, WorldMapLayer.class));
 
     layerPanel = widget(new LayerPanel(wwd, null), constraints:BorderLayout.WEST)
 
+}
+
+annotationLayer = new AnnotationLayer()
+wwd.model.layers.add(0, annotationLayer)
+
+inset = 10; // pixels
+def addTweet(Position position, String tweet, Image tweetImage) {
+
+    def image = new BufferedImage(tweetImage.width, tweetImage.height, BufferedImage.TYPE_INT_ARGB)
+    image.getGraphics().drawImage(tweetImage, 0, 0, null)
+    int width = image.width
+    int height = image.height
+    // Check for alpha channel in image
+    // Create annotation
+
+    def text = tweet
+    GlobeAnnotation ga = new GlobeAnnotation(text, position)
+    bean(ga.attributes,
+        insets: [inset, height + inset * 2, inset, inset],
+        imageSource: image,
+        imageOffset: [inset, inset],
+        imageRepeat: Annotation.IMAGE_REPEAT_NONE,
+        imageOpacity: 1,
+        size: [width + inset * 2 + 200, 68],
+        adjustWidthToText : Annotation.SIZE_FIT_TEXT,
+        backgroundColor : Color.WHITE,
+        textColor: Color.BLACK,
+        borderColor: Color.BLACK)
+
+    annotationLayer.addAnnotation(ga)
 }
