bb6eca94b1147be1264b333fbdfacf3a7f997f04
U-Mintaka\Danno <Danno@Mintaka.(none)>
2009-11-21 23:23:57 -0700

    griffon 0.2 upgrade

--- griffon-app/conf/Application.groovy
+++ griffon-app/conf/Application.groovy
@@ -1,19 +1,12 @@
 application {
-    title='Twittersphere'
-    startupGroups = ['twittersphere']
-
-    // Should Griffon exit when no Griffon created frames are showing?
-    autoShutdown = true
-
-    // If you want some non-standard application class, apply it here
-    //frameClass = 'javax.swing.JFrame'
+	title="Twittersphere"
+	startupGroups=["twittersphere"]
+	autoShutdown=true
 }
 mvcGroups {
-    // MVC Group for "twittersphere"
-    'twittersphere' {
-        model = 'TwittersphereModel'
-        view = 'TwittersphereView'
-        controller = 'TwittersphereController'
-    }
-
+	twittersphere {
+		model="TwittersphereModel"
+		view="TwittersphereView"
+		controller="TwittersphereController"
+	}
 }
